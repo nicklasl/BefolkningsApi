dd3b75231ad2ec1791578afd60b30aecdfe3f18a
Nicklas <nicklaslundin@gmail.com>
2014-11-16 21:27:37 +0100

    new dependencies

--- build.sbt
+++ build.sbt
@@ -6,5 +6,7 @@ scalaVersion := "2.11.4"
 
 libraryDependencies ++= Seq(
   "com.typesafe.play" %% "play-ws" % "2.4.0-M1",
-  "com.typesafe.akka" %% "akka-actor" % "2.3.7"
+  "com.typesafe.akka" %% "akka-actor" % "2.3.7",
+  "org.specs2" %% "specs2" % "2.4.2" % "test",
+  "com.typesafe.akka" %% "akka-testkit" % "2.3.7" % "test"
 )
--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,7 +1,5 @@
 import nu.nldv.befolkning.Api
-import nu.nldv.befolkning.model.Income
 
-import scala.concurrent.Future
 import scala.io.Source
 
 object Application extends App {
@@ -17,12 +15,12 @@ object Application extends App {
       actorSystem.shutdown()
   }
 
-//  private val eventualIncomes: Future[List[Income]] = Api.getIncomeData(apikey)
-//
-//  eventualIncomes map {
-//    x =>
-//      x.sortBy(f => f.averageIncomeWithoutZeroEarners).takeRight(10).foreach(println(_))
-//      actorSystem.shutdown()
-//  }
+  //  private val eventualIncomes: Future[List[Income]] = Api.getIncomeData(apikey)
+  //
+  //  eventualIncomes map {
+  //    x =>
+  //      x.sortBy(f => f.averageIncomeWithoutZeroEarners).takeRight(10).foreach(println(_))
+  //      actorSystem.shutdown()
+  //  }
 
 }
